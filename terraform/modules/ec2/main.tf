141fa48109f7358070c66aee778deb3ed7f08830
AvilashBhowmick12 <avilash.12bhowmick@gmail.com>
2026-05-25 15:30:30 +0000

    modified ebs volume for terraform and ansible wordpress flow

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -7,7 +7,15 @@ resource "aws_instance" "this" {
   associate_public_ip_address = true
   iam_instance_profile        = var.iam_instance_profile
 
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = var.root_volume_size
+    delete_on_termination = true
+  }
+
   tags = {
     Name = var.name
   }
 }
+
+
--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -27,3 +27,9 @@ variable "iam_instance_profile" {
   description = "IAM instance profile name to attach to EC2"
   type        = string
 }
+
+variable "root_volume_size" {
+  description = "Root EBS volume size in GB"
+  type        = number
+  default     = 20
+}
